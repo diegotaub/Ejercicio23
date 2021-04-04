05c987875bd24ad1fee776051cb8e9e03ca08ed7
diego.taub <diego.taub@accenture.com>
2021-04-04 20:51:59 -0300

    Ejercicio23

---
+++ Ejercicio23/Ejercicio23/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese una fecha: ");
+            if(!DateTime.TryParse(Console.ReadLine(), out DateTime Fecha)){
+                Console.Write("La fecha ingresada no es válida");
+            }
+            else
+            {
+                Console.Write("La fecha ingresada es válida.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
